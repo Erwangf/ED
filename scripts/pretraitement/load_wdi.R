cc21a16f89712a853c53bc42add7674c5cdd788a
Erwangf <erwangf@gmail.com>
2018-02-28 16:36:48 +0100

    wdi : better

--- scripts/pretraitement/load_wdi.R
+++ scripts/pretraitement/load_wdi.R
@@ -11,6 +11,11 @@ library(RPostgreSQL)
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, user='postgres', password='admin123', dbname='postgres', host='127.0.0.1')
 
+df <- data.frame(area=character(),year=numeric())
+df$area = as.character(df$area)
+for(id in indicatorTypes){
+  df[id] = numeric()
+}
 
 
 # indicatorsDF$Value.Footnotes = NULL
@@ -19,13 +24,36 @@ pb <- txtProgressBar(min=0,max=length(filePaths), initial=0,style=3)
 for( i in 1:length(filePaths)){
   setTxtProgressBar(pb,i)
   tmpData <- read.table(paste(indicatorFolder,filePaths[i],sep = ""), header=T, quote="\"", sep=";",fill = T)[,1:3]
+  tmpData$Country.or.Area <-  as.character(tmpData$Country.or.Area)
+  if(class(tmpData$Year)=="factor"){
+    tmpData$Year <-  as.numeric(levels(tmpData$Year)[as.numeric(tmpData$Year)])
+  }
+  
+  
   tableName <- indicatorTypes[i]
   
-  # Drop table if it already exists
-  if (dbExistsTable(con, tableName)) dbRemoveTable(con, tableName)
+  for(j in 1:nrow(tmpData)){
+    line = tmpData[j,]
+    
+    if(nrow(df[df$area==line[,1] & df$year==line[,2],])==0){
+      df[nrow(df)+1,] <- c(area=line[,1],year=line[,2], NA, NA, NA, NA, NA, NA, NA, NA, NA)
+    }
+    if(is.na(line[,3])){
+      print(line)
+    } else {
+      df[df$area==line[,1] & df$year==line[,2],i+2] <- line[,3]
+    }
+    
+  }
   
-  dbWriteTable(con, name = tableName, value = tmpData, row.names = FALSE)
+
 }
+
+# Drop table if it already exists
+if (dbExistsTable(con, "wdi")) dbRemoveTable(con, tableName)
+
+dbWriteTable(con, name = "wdi", value = df, row.names = FALSE)
+
 cat("Finished import : ")
 cat(paste(indicatorTypes),sep="\n")
 cat("End of programm.")
